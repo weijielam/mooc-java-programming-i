7b57a5cc2cae70af487b8ee0822c2cee75943fb8
Wei Jie Lam <weijielam@Weis-MacBook-Pro-2.local>
2022-08-15 20:38:24 +0100

    Completed up to Part01_23.SimpleCalculator

--- part01-Part01_23.SimpleCalculator/src/main/java/SimpleCalculator.java
+++ part01-Part01_23.SimpleCalculator/src/main/java/SimpleCalculator.java
@@ -7,6 +7,20 @@ public class SimpleCalculator {
         Scanner scanner = new Scanner(System.in);
 
         // Write your program here
+        System.out.println("Give the first number:");
+        int first = Integer.valueOf(scanner.nextLine());
+        System.out.println("Give the second number:");
+        int second = Integer.valueOf(scanner.nextLine());
+        
+        int addition = first + second;
+        int subtraction = first - second;
+        int multiplication = first * second;
+        double division = (first / (second * 1.0));
+        
+        System.out.println(first + " + " + second + " = " + addition);
+        System.out.println(first + " - " + second + " = " + subtraction);
+        System.out.println(first + " * " + second + " = " + multiplication);
+        System.out.println(first + " / " + second + " = " + division);
 
     }
 }
